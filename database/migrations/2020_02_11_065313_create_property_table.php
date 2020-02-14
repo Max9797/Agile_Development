fd202a48742c591bd910f282e66fcccdad1c49d5
fikri <fikri_jwahid@yahoo.com>
2020-02-14 09:52:19 +0800

    laravel

---
+++ database/migrations/2020_02_11_065313_create_property_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePropertyTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('property', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('property_name');
+            $table->string('gender');
+            $table->string('city');
+            $table->integer('price');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('property');
+    }
+}
